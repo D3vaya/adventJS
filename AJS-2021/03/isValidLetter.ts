326c6747bf6d4f358a20e1a15edd403a6d4d0175
Jc Ayala <j.ayala.higuera@gmail.com>
2023-03-15 19:51:27 -0300

    feat: complete challenge 3

--- AJS-2021/03/isValidLetter.ts
+++ AJS-2021/03/isValidLetter.ts
@@ -1,6 +1,51 @@
 export const isValidLetter = (letter: string) => {
-  if(!letter) throw "undefined parameter";
-  if(typeof letter !== 'string') throw "el parámetro debe ser string";
-  
+  if (!letter) throw "undefined parameter";
+  if (typeof letter !== "string") throw "el parámetro debe ser string";
 
-}
+  const REGEX_NOT_KEYANDBRACKETS = /^[^\[\]\{\}]*$/g;
+  const REGEX_CLOSED_PARENTHESES =/^([^\(\)]*|\(([^()]|\([^\(\)]*\))*\))*$/;
+  if (!REGEX_NOT_KEYANDBRACKETS.test(letter)) return false;
+  if (letter.replace(/\s+/g, "").includes("()")) return false;
+  if (!REGEX_CLOSED_PARENTHESES.test(letter)) return false;
+
+  const arrParentheses = letter
+    .replace(/\s+/g, "")
+    .split("")
+    .filter((l) => l === "(" || l === ")");
+
+  let open = 0;
+  let close = 0;
+
+  arrParentheses.forEach((p) => {
+    if (p === "(") open++;
+    if (p === ")") close++;
+  });
+
+  return open === close;
+};
+
+// "bici coche (balón) bici coche peluche" // -> ✅
+// "(muñeca) consola bici" // ✅
+
+// "bici coche (balón bici coche" // -> ❌
+// "peluche (bici [coche) bici coche balón" // -> ❌
+// "(peluche {) bici" // -> ❌
+// "() bici" // ❌
+// "bici (balón bici coche" // ❌
+// "(a) (b) (c)" // ✅
+export const letters = [
+  "bici coche (balón) bici coche peluche",
+  "(muñeca) consola bici",
+  "bici coche (balón bici coche",
+  "peluche (bici [coche) bici coche balón",
+  "(peluche {) bici",
+  "() bici",
+  "bici (balón bici coche",
+  "(a) (b) (c)",
+  "(a() bici (a)",
+  ")bici( casa play",
+];
+
+letters.forEach((letter) => {
+  isValidLetter(letter);
+});
---
+++ AJS-2021/__test__/03/isValidLetter.test.js
@@ -0,0 +1,34 @@
+import { isValidLetter } from "../../03/isValidLetter";
+// "bici coche (balón) bici coche peluche" // -> ✅
+// "(muñeca) consola bici" // ✅
+
+// "bici coche (balón bici coche" // -> ❌
+// "peluche (bici [coche) bici coche balón" // -> ❌
+// "(peluche {) bici" // -> ❌
+// "() bici" // ❌
+
+describe("Pruebas desafío numero 3", () => {
+  it("debe lanzar error si falta el primer parámetro", () => {
+    expect(() => isValidLetter()).toThrowError();
+  });
+
+  it("debe lanzar error si el parametro no es estring", () => {
+    expect(() => isValidLetter(2)).toThrowError();
+  });
+
+  it("debe retornar false si contiene una { o [", () => {
+    expect(isValidLetter("(a { bici (a)")).toEqual(false);
+  });
+
+  it("debe retornar false si existe un parentesis sin cerrar ", () => {
+    expect(isValidLetter("(a bici (a)")).toEqual(false);
+  });
+
+  it("debe retornar false si existe un parentesis vacio ", () => {
+    expect(isValidLetter("() bici muñeca")).toEqual(false);
+  });
+
+  it("debe retornar false si un parentesis no tiene apertura y cierre correcto ", () => {
+    expect(isValidLetter(")bici( casa play")).toEqual(false);
+  });
+});
