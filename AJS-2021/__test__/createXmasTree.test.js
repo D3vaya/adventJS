d13c9ccccb3335b6156414f3ffae893b14d5ef5c
Jc Ayala <j.ayala.higuera@gmail.com>
2023-03-16 10:35:56 -0300

    feat: test challange 5

---
+++ AJS-2021/__test__/createXmasTree.test.js
@@ -0,0 +1,15 @@
+import { createXmasTree } from "../04/createXmasTree";
+describe("Pruebas desafío numero 4", () => {
+  it("debe lanzar error si falta el parámetro", () => {
+    expect(() => createXmasTree()).toThrowError();
+  });
+  it("debe lanzar error si el parámetro no es numerico", () => {
+    expect(() => createXmasTree("2")).toThrowError();
+  });
+  it("debe lanzar error si el parámetro numerico es mayor a 100", () => {
+    expect(createXmasTree(101)).toBeFalsy();
+  });
+  it("debe lanzar error si el parámetro numerico es menor a 1", () => {
+    expect(createXmasTree(-3)).toEqual(false);
+  });
+});
