efd9ed4cba96104ddcafaea2fca75149b0a4407a
Jc Ayala <j.ayala.higuera@gmail.com>
2023-03-17 10:55:31 -0300

    feat: complete challange 9

--- AJS-2021/09/groupBy.ts
+++ AJS-2021/09/groupBy.ts
@@ -1,1 +1,26 @@
-export const groupBy = (collection:any[], it:any) =>{}
+// export const groupBy = (collection: any[], it: any) => {
+//   let acc = {};
+//   collection.map((el) => {
+//     let prop = typeof it === "function" ? it(el) : el[it];
+//     acc[prop] = acc[prop] ? acc[prop].concat(el): [el]
+//   });
+//   return acc;
+// };
+
+export function groupBy2(list: any[], keyGetter: any) {
+  const map = new Map();
+  list.forEach((item) => {
+    const key =
+      typeof keyGetter === "function" ? keyGetter(item) : item[keyGetter];
+    const collection = map.get(key);
+    if (!collection) {
+      map.set(key, [item]);
+    } else {
+      collection.push(item);
+    }
+  });
+  return map;
+}
+
+console.log(groupBy([6.1, 4.2, 6.3], Math.floor));
+//[6.1, 4.2, 6.3] con Math.floor debería ser { 6: [6.1, 6.3], 4: [4.2] }
--- AJS-2021/__test__/groupBy.test.js
+++ AJS-2021/__test__/groupBy.test.js
@@ -1,3 +1,7 @@
-describe('Pruebas desafío 9', () => {
+import { groupBy } from "../09/groupBy.ts";
+describe("Pruebas desafío 9", () => {
+  it("debe arrojar error si no vienen ambos parámetros", () => {
+    expect(() => groupBy()).toThrowError();
+  });
   
 });
