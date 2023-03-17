1b9b7b0a30a8b474bdb43eb93e1141adef525ae6
Jc Ayala <j.ayala.higuera@gmail.com>
2023-03-17 09:48:03 -0300

    feat: complete challenge 8

--- AJS-2021/08/maxProfit.ts
+++ AJS-2021/08/maxProfit.ts
@@ -1,4 +1,20 @@
-export const maxProfit = (prices: number[]) =>{
-  
-  return prices
-}
+export const maxProfit = (prices: number[]) => {
+  if (!prices) throw "undefined parameters";
+  if (!Array.isArray(prices)) throw "parameter is not a array";
+
+  const halfPrices = Math.ceil(prices.length / 2);
+  const leftHalfPrices = prices.slice(0, halfPrices);
+  const rightHalfPrices = prices.slice(halfPrices, prices.length);
+
+  const minValue = Math.min(...leftHalfPrices);
+  const maxValue = Math.max(...rightHalfPrices);
+
+  if (minValue >= maxValue) return -1;
+  return maxValue - minValue;
+};
+
+console.log({ expectd: 16, result: maxProfit([39, 18, 29, 25, 34, 32, 5]) });
+console.log({ expectd: 60, result: maxProfit([10, 20, 30, 40, 50, 60, 70]) });
+
+// console.log({expectd: -1, result: maxProfit([18, 15, 12, 11, 9, 7])})
+// console.log({expectd: -1, result: maxProfit([3, 3, 3, 3, 3])})
--- AJS-2021/__test__/maxProfit.test.js
+++ AJS-2021/__test__/maxProfit.test.js
@@ -1,3 +1,15 @@
-describe('Pruebas desafío 8', () => {
-  
+import { maxProfit } from "../08/maxProfit.ts";
+describe("Pruebas desafío 8", () => {
+  it("debe lanzar error si no viene el parámetro ", () => {
+    expect(() => maxProfit()).toThrowError();
+  });
+  it("debe lanzar error si el parámetro no es un arreglo ", () => {
+    expect(() => maxProfit("ws")).toThrowError();
+  });
+  it("debe retornar -1 si el dia no presenta ganancia", () => {
+    expect(maxProfit([18, 15, 12, 11, 9, 7])).toEqual(-1);
+  });
+  it("debe retornar la ganancia del dia. Este dato debe ser numerico", () => {
+    expect(maxProfit([39, 18, 29, 25, 34, 32, 5])).toEqual(16);
+  });
 });
