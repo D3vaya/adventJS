68e6b7edb2a090396dbd21bdb8f06515aca0e8ea
Jc Ayala <j.ayala.higuera@gmail.com>
2023-03-17 20:09:15 -0300

    feat: complete challenge 12

--- AJS-2021/12/getMinJump.ts
+++ AJS-2021/12/getMinJump.ts
@@ -1,3 +1,14 @@
-export const getMinJump =(obstacles:number[])=> {
-  return 0;
-}
+export const getMinJump = (obstacles: number[]) => {
+  if (!Array.isArray(obstacles)) throw "parameter nota a array";
+  if (obstacles.includes(0)) throw "obstacle in position 0";
+
+  let result = 2;
+  let bool = true;
+  let prevResult = null;
+
+  while (bool) {
+    obstacles.map((obstacle) => !(obstacle % result) && result++);
+    result === prevResult ? (bool = false) : (prevResult = result);
+  }
+  return result;
+};
--- AJS-2021/__test__/getMinJump.test.js
+++ AJS-2021/__test__/getMinJump.test.js
@@ -1,3 +1,16 @@
-describe('Pruebas desafío 12', () => {
-  
+import { getMinJump } from "../12/getMinJump.ts";
+describe("Pruebas desafío 12", () => {
+  it("debe lanzar error si no viene el parámetro", () => {
+    expect(() => getMinJump()).toThrowError();
+  });
+  it("debe lanzar error si el parámetro no es un arreglo", () => {
+    expect(() => getMinJump(2)).toThrowError();
+  });
+  it("debe lanzar error si la posición 0 tiene un obstaculo. ", () => {
+    expect(() => getMinJump([0, 3, 6, 7, 9])).toThrowError();
+  });
+  it("debe lanzar error si el retorno no es un numero. ", () => {
+    const r = getMinJump([ 3, 6, 7, 9]);
+    expect(typeof r).toEqual("number")
+  });
 });
