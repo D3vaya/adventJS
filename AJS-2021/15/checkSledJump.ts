00c99a3ff1806dea19bffb5b384d64462003eed9
Jc Ayala <j.ayala.higuera@gmail.com>
2023-03-17 22:06:09 -0300

    feat: challange 16

---
+++ AJS-2021/15/checkSledJump.ts
@@ -0,0 +1,4 @@
+export const checkSledJump = (heights: number[]) => {
+  // ¡No olvides compartir tu solución en redes!
+  return false;
+};
---
+++ AJS-2021/16/decodeNumber.ts
@@ -0,0 +1,4 @@
+export const decodeNumber = (symbols: string) => {
+  // ¡No olvides compartir tu solución en redes!
+  return false;
+};
