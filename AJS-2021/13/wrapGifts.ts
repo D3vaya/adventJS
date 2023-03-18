9ed2a983b71b55ec64bb4a7186966501b588d2e1
Jc Ayala <j.ayala.higuera@gmail.com>
2023-03-17 21:05:42 -0300

    feat: complete challange 13

--- AJS-2021/13/wrapGifts.ts
+++ AJS-2021/13/wrapGifts.ts
@@ -1,3 +1,43 @@
-export const wrapGifts = (gifts:string[]) => {
-  return gifts
-}
+export const wrapGifts = (gifts: string[]) => {
+  if (!Array.isArray(gifts)) throw "parameter is not a array";
+  if (gifts.length === 0) return [];
+
+  if (gifts.length > 1 && gifts[0].length !== gifts[1].length)
+    throw "La longitud de las posiciones del arreglo no es la misma";
+
+  let result: string[] = [];
+  let lengthMaxEmoji = Math.min(...gifts.map((emoji) => lengthEmoji(emoji)));
+
+  gifts.forEach((g, i) => {
+    if (i === 0) result.push("*".repeat(lengthMaxEmoji * 2 + 2));
+    result.push(`*${g}*`);
+    if (i === gifts.length - 1) result.push("*".repeat(lengthMaxEmoji * 2 + 2));
+  });
+
+  return result;
+};
+
+const lengthEmoji = (emoji: string) => {
+  let count = 0;
+
+  for (let char of emoji) {
+    count++;
+    console.log(char);
+  }
+  return count;
+};
+
+// `/* Resultado:
+// [ '****',
+//   '*📷*',
+//   '*⚽️*',
+//   '****'
+// ]
+// */`
+
+console.log(wrapGifts(["🏈🎸", "🎮🧸"]));
+// console.log(wrapGifts(["📷"]));
+console.log(wrapGifts(["📷", "⚽️"]));
+
+//['📷', '⚽️'] sería así ['****', '*📷*', '*⚽️*', '****']
+//['🏈🎸', '🎮🧸'] no lo estás envolviendo bien...
--- AJS-2021/__test__/wrapGifts.test.js
+++ AJS-2021/__test__/wrapGifts.test.js
@@ -1,3 +1,24 @@
-describe('Pruebas desafio 13', () => {
-  
+import { wrapGifts } from "../13/wrapGifts.ts";
+describe("Pruebas desafio 13", () => {
+  it("debe lanzar error si el parámetro no eviene", () => {
+    expect(() => wrapGifts()).toThrowError();
+  });
+
+  it("debe lanzar error si el parámetro no es un arreglo", () => {
+    expect(() => wrapGifts("w")).toThrowError();
+  });
+
+  it("Si el arreglo de regalos esta vacío, retornar un arreglo vació", () => {
+    expect(wrapGifts([])).toEqual([]);
+  });
+
+  it("Cada posición del arreglo debe tener la misma longitud", () => {
+    const t = ["🏈🎸", "🎮🧸"];
+    const expected = t[0].length !== t[1].length;
+    expect(expected).toBeFalsy();
+  });
+  it("should ", () => {
+    const expected = wrapGifts(["📷", "⚽️"]);
+    expect(expected).toEqual(['****', '*📷*', '*⚽️*', '****'])
+  });
 });
