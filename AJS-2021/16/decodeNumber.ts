e8d054a6e3e8f12526dbffd8fc19aadb06041ace
Jc Ayala <j.ayala.higuera@gmail.com>
2023-03-19 04:30:14 -0300

    feat: complete challange 16

--- AJS-2021/16/decodeNumber.ts
+++ AJS-2021/16/decodeNumber.ts
@@ -1,4 +1,27 @@
 export const decodeNumber = (symbols: string) => {
-  // ¡No olvides compartir tu solución en redes!
-  return false;
+  if (typeof symbols !== "string") throw "error parameter";
+
+  const map: Map<string, number> = new Map();
+  map.set(".", 1);
+  map.set(",", 5);
+  map.set(":", 10);
+  map.set(";", 50);
+  map.set("!", 100);
+
+  //const currentSymbols = [".", ",", ":", ";", "!"];
+
+  let acc = 0;
+  for (let index = 0; index < [...symbols].length; index++) {
+    let nextSymbol = map.get(symbols[index + 1])!;
+    const value = map.get(symbols[index])!;
+    if (value < nextSymbol) acc = acc - value;
+    else acc = value + acc;
+  }
+
+  return acc;
 };
+
+console.log({ expected: 107, result: decodeNumber(".........!") });
+console.log({ expected: 5, result: decodeNumber("..,") });
+console.log({ expected: 95, result: decodeNumber("..,!") });
+console.log({ expected: 49, result: decodeNumber(".;!") });
--- AJS-2021/__test__/decodeNumber.test.js
+++ AJS-2021/__test__/decodeNumber.test.js
@@ -1,3 +1,53 @@
-describe('Name of the group', () => {
-  
+import { decodeNumber } from "../16/decodeNumber.ts";
+describe("Pruebas desafío 16", () => {
+  it("Debe lanzar error si el parámetro no viene", () => {
+    expect(() => decodeNumber()).toThrowError();
+  });
+  it("Debe lanzar error si el parámetro no es string", () => {
+    expect(() => decodeNumber(2)).toThrowError();
+  });
+
+  it("Debe retornar NaN si e simbolo no esta identificado", () => {
+    const expected = decodeNumber(";.W");
+    expect(expected).toBeNaN();
+  });
+});
+
+describe("Casos de prueba", () => {
+  it("Prueba 1  | ... -> 3", () => {
+    expect(decodeNumber("...")).toEqual(3);
+  });
+  it("Prueba 2  | ., -> 4", () => {
+    expect(decodeNumber(".,")).toEqual(4);
+  });
+  it("Prueba 3  | ,. -> 6", () => {
+    expect(decodeNumber(",.")).toEqual(6);
+  });
+  it("Prueba 4  | ,... -> 8", () => {
+    expect(decodeNumber(",...")).toEqual(8);
+  });
+  it("Prueba 5  | .........! -> 107", () => {
+    expect(decodeNumber(".........!")).toEqual(107);
+  });
+  it("Prueba 6  | .; -> 49", () => {
+    expect(decodeNumber(".;")).toEqual(49);
+  });
+  it("Prueba 7  | .., -> 5", () => {
+    expect(decodeNumber("..,")).toEqual(5);
+  });
+  it("Prueba 8  | ..,! -> 95", () => {
+    expect(decodeNumber("..,!")).toEqual(95);
+  });
+  it("Prueba 9  | .;! -> 49", () => {
+    expect(decodeNumber(".;!")).toEqual(49);
+  });
+  it("Prueba 11 | !!! -> 300", () => {
+    expect(decodeNumber("!!!")).toEqual(300);
+  });
+  it("Prueba 12 | ;! -> 50", () => {
+    expect(decodeNumber(";!")).toEqual(50);
+  });
+  it("Prueba 12 | ;.W -> NaN", () => {
+    expect(decodeNumber(";.W")).toBeNaN();
+  });
 });
