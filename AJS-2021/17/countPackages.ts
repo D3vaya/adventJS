72d9fadcc1bc88493eca53571b37919c10d9bec4
Jc Ayala <j.ayala.higuera@gmail.com>
2023-03-19 23:30:17 -0300

    feat:  init challange 17

--- AJS-2021/17/countPackages.ts
+++ AJS-2021/17/countPackages.ts
@@ -1,3 +1,83 @@
-export const countPackages = (carriers: [], carrierID: string) => {
-  return 0
+export const countPackages = (
+  carriers: (string | number | string[])[][],
+  carrierID: string
+) => {
+  if (!carriers || !carrierID) throw "undefined parameters";
+  if (!Array.isArray(carriers)) throw "parameter is not a array";
+  if (typeof carrierID !== "string") throw "parameter is not a string";
+  if (carriers.length === 0) return 0;
+
+  const selectedCarrier = carriers.find((c) => c[0] === carrierID) as (
+    | string
+    | number
+    | string[]
+  )[][];
+  if (!selectedCarrier) return 0;
+  //const employeesIDs: string[] = selectedCarrier[2] as string[];
+
+  //const employees = carriers.filter((c) => employeesIDs.includes(String(c[0])));
+  //let counter: number = selectedCarrier[1] as number;
+  // employees.forEach((arr) => {
+  //   const [transportist, packages, employees] = arr;
+  //   console.log("[LOG] 🚧 ", { transportist, packages, employees });
+  //   counter += Number(packages);
+  // });
+  //let counter = Number(selectedCarrier[1]);
+  return sumPackageEmployees(selectedCarrier, carriers, 0);
 };
+
+export const sumPackageEmployees = (
+  employees: (string | number | string[])[][],
+  carriers: (string | number | string[])[][],
+  counter: number
+) => {
+  let prevCounter = counter
+  if (employees.length === 3) {
+    const [transportist, packages, employeesChild] = employees;
+    transportist;
+    //const packages = arr[1] as number;
+    const emp = employeesChild as unknown as (string | number | string[])[][];
+    counter += Number(packages);
+    console.log("+", { packages, prevCounter, counter  });
+
+    if (emp.length > 0) {
+      sumPackageEmployees(emp, carriers, counter);
+    }
+  } else {
+    employees.forEach((carrierID) => {
+      const sc = carriers.find((c) => c[0] === carrierID) as (
+        | string
+        | number
+        | string[]
+      )[][];
+      if (sc) {
+        sumPackageEmployees(sc, carriers, counter);
+      }
+    });
+  }
+
+  return counter;
+};
+
+export const carriers = [
+  ["dapelu", 5, ["midu", "jelowing"]],
+  ["midu", 2, []],
+  ["jelowing", 2, []],
+];
+
+//console.log("[LOG] 🚧 ", countPackages(carriers, "dapelu"));
+// type Carrier = {
+//   carrierID: string;
+//   packages: number;
+//   employees: string[];
+// };
+export const carriers2 = [
+  ["lolivier", 8, ["camila", "jesuspoleo"]],
+  ["camila", 5, ["sergiomartinez", "conchaasensio"]],
+  ["jesuspoleo", 4, []],
+  ["sergiomartinez", 4, []],
+  ["conchaasensio", 3, ["facundocapua", "faviola"]],
+  ["facundocapua", 2, []],
+  ["faviola", 1, []],
+];
+console.log("[LOG] 🚧 ", countPackages(carriers2, "camila"));
--- AJS-2021/__test__/countPackages.test.js
+++ AJS-2021/__test__/countPackages.test.js
@@ -1,2 +1,51 @@
 import { countPackages } from "../17/countPackages.ts";
-describe("Pruebas desafío 17", () => {});
+describe("Pruebas desafío 17", () => {
+  it("Debe lanzar error si los parámetros no vienen", () => {
+    expect(() => countPackages()).toThrowError();
+  });
+
+  it("Debe lanzar error si el primer parámetro no es un arreglo", () => {
+    expect(() => countPackages(1, "kjh")).toThrowError();
+  });
+
+  it("Debe lanzar error si el segundo parámetro no es un string", () => {
+    const carriers = [
+      ["dapelu", 5, ["midu", "jelowing"]],
+      ["midu", 2, []],
+      ["jelowing", 2, []],
+    ];
+    expect(() => countPackages(carriers, 2)).toThrowError();
+  });
+
+  it("Debe lanzar 0 si el arreglo de carriers viene vacio", () => {
+    expect(countPackages([], "kjh")).toEqual(0);
+  });
+});
+
+describe("Casos de prueba", () => {
+  it("Caso de prueba 1", () => {
+    const carriers = [
+      ["dapelu", 5, ["midu", "jelowing"]],
+      ["midu", 2, []],
+      ["jelowing", 2, []],
+    ];
+
+    const expected = countPackages(carriers, "dapelu");
+    expect(expected).toEqual(9);
+  });
+
+  it("Caso de prueba 2 ", () => {
+    const carriers2 = [
+      ["lolivier", 8, ["camila", "jesuspoleo"]],
+      ["camila", 5, ["sergiomartinez", "conchaasensio"]],
+      ["jesuspoleo", 4, []],
+      ["sergiomartinez", 4, []],
+      ["conchaasensio", 3, ["facundocapua", "faviola"]],
+      ["facundocapua", 2, []],
+      ["faviola", 1, []],
+    ];
+
+    const expected = countPackages(carriers2, "camila");
+    expect(expected).toEqual(15);
+  });
+});
