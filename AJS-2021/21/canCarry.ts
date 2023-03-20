1338860c694e27fdea21c379e536d8b982c8c65a
Jc Ayala <j.ayala.higuera@gmail.com>
2023-03-20 15:06:50 -0300

    feat: challange 21 complete

--- AJS-2021/21/canCarry.ts
+++ AJS-2021/21/canCarry.ts
@@ -1,3 +1,46 @@
 export const canCarry = (capacity: number, trip: number[][]) => {
-  return false
-}
+  if (typeof capacity !== "number" && !Array.isArray(trip))
+    throw "error parameter";
+  if (typeof capacity !== "number") throw "capacity is not a number";
+  if (!Array.isArray(trip[0])) throw "trip is not a Array";
+  if (typeof trip[0][0] !== "number") throw "trip is not a Array numbers";
+
+  let counterGifts = 0;
+
+  for (let index = 0; index < trip.length; index++) {
+    const [numberGiftsPK, pickUpPointPK, deliveryPointPK] = trip[index];
+    counterGifts += numberGiftsPK;
+
+    if (typeof trip[index + 1] !== "undefined") {
+      const pickUpPointNext = trip[index + 1][1];
+      if (deliveryPointPK <= pickUpPointNext) counterGifts -= numberGiftsPK;
+    }
+  }
+  if (counterGifts <= capacity) return true;
+
+  return false;
+};
+
+// console.log({
+//   expected: false,
+//   result: canCarry(4, [
+//     [2, 5, 8],
+//     [3, 6, 10],
+//   ]),
+// });
+
+// console.log({
+//   expected: true,
+//   result: canCarry(4, [
+//     [2, 3, 8],
+//     [2, 5, 7],
+//   ]),
+// });
+
+console.log({
+  expected: true,
+  result: canCarry(3, [
+    [2, 1, 5],
+    [3, 5, 7],
+  ]),
+});
--- AJS-2021/__test__/canCarry.test.js
+++ AJS-2021/__test__/canCarry.test.js
@@ -1,4 +1,57 @@
 import { canCarry } from "../21/canCarry.ts";
-describe('Pruebas desafío 21', () => {
-  
+describe("Pruebas desafío 21", () => {
+  it("debe lanzar error si no vienen los parámetros", () => {
+    expect(() => canCarry()).toThrowError();
+  });
+  it("debe lanzar error si el primer parámetro no es un número", () => {
+    expect(() =>
+      canCarry("2", [
+        [23, 4, 5],
+        [3, 6, 7],
+      ])
+    ).toThrowError();
+  });
+  it("debe lanzar error si el segundo parámetro no es un arreglo de arreglos", () => {
+    expect(() =>
+      canCarry(2, [
+        '2',
+        [3, 6, 7],
+      ])
+    ).toThrowError();
+  });
+  it("debe lanzar error si el segundo parámetro no es un arreglo de arreglos de numeros", () => {
+    expect(() =>
+      canCarry(2, [
+        ['23', '4', '5'],
+        ['3', '6', '7'],
+      ])
+    ).toThrowError();
+  });
+});
+
+describe('Casos de prueba', () => {
+  it('Caso 1', () => {
+    const expected = canCarry(4, [[2, 5, 8], [3, 6, 10]]);
+    expect(expected).toBeFalsy()
+  });
+  it('Caso 2', () => {
+    const expected = canCarry(3, [[1, 1, 5], [2, 2, 10]])
+    expect(expected).toBeTruthy()
+  });
+  it('Caso 3', () => {
+    const expected = canCarry(3, [[2, 1, 5],[3, 5, 7]])
+    expect(expected).toBeTruthy()
+  });
+  it('Caso 4', () => {
+    const expected = canCarry(4, [[2, 3, 8],[2, 5, 7]])
+    expect(expected).toBeTruthy()
+  });
+  it('Caso 5', () => {
+    const expected = canCarry(1, [[2, 3, 8]])
+    expect(expected).toBeFalsy()
+  });
+  it('Caso 6', () => {
+    const expected = canCarry(2, [[1, 2, 4], [2, 3, 8]])
+    expect(expected).toBeFalsy()
+  });
 });
