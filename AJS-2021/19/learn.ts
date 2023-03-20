43c2a940e218b074cd2a811cc4fec24ff1a785ab
Jc Ayala <j.ayala.higuera@gmail.com>
2023-03-20 11:02:14 -0300

    feat: challange 19 complete

--- AJS-2021/19/learn.ts
+++ AJS-2021/19/learn.ts
@@ -1,3 +1,32 @@
 export const learn = (time: number, courses: number[]) => {
-  return [0, 0];
+  if (typeof time !== "number" || !Array.isArray(courses))
+    throw "error parameter";
+  if (courses.length === 0) return [0, 0];
+
+  let tmpValue = 0;
+  let result: number[] = [];
+  let tmpSuccessValue = 0;
+  for (let i = 0; i < courses.length; i++) {
+    const hoursPerCourse = courses[i];
+    for (let j = i + 1; j < courses.length; j++) {
+      const hoursPerCourse2 = courses[j];
+      tmpValue = hoursPerCourse + hoursPerCourse2;
+
+      if (tmpValue <= time && tmpValue > tmpSuccessValue) {
+        result = [i, j];
+        tmpSuccessValue = tmpValue;
+      }
+    }
+  }
+  if (result.length === 0) return null;
+  return result;
 };
+
+console.log({ expected: [0, 2], result: learn(10, [2, 3, 8, 1, 4]) });
+console.log({ expected: [1, 2], result: learn(15, [2, 10, 4, 1]) });
+console.log({ expected: [0, 1], result: learn(25, [10, 15, 20, 5]) });
+console.log({ expected: [1, 2], result: learn(8, [8, 2, 1]) });
+
+console.log({ expected: [3, 4], result: learn(8, [8, 2, 1, 4, 3]) });
+console.log({ expected: null, result: learn(4, [10, 14, 20]) });
+console.log({ expected: null, result: learn(5, [5, 5, 5]) });
--- AJS-2021/__test__/learn.test.js
+++ AJS-2021/__test__/learn.test.js
@@ -1,2 +1,42 @@
 import { learn } from "../19/learn.ts";
-describe("Pruebas desafío 19", () => {});
+describe("Pruebas desafío 19", () => {
+  it("debe lanzar error si no van los parámetros", () => {
+    expect(() => learn()).toThrowError();
+  });
+
+  it("debe lanzar error si el primer parámetro no es un numero", () => {
+    expect(() => learn("2", [])).toThrowError();
+  });
+
+  it("debe lanzar error si el segundo parámetro no es un arreglo", () => {
+    expect(() => learn(1, 2)).toThrowError();
+  });
+
+  it("debe retornar [0, 0] si el segundo parámetro viene vacio", () => {
+    const expected = learn(1, []);
+    expect(expected).toEqual([0, 0]);
+  });
+
+  
+});
+
+describe("Casos de prueba", () => {
+  it("caso 1", () => {
+    const expected = learn(10, [2, 3, 8, 1, 4]);
+    expect(expected).toEqual([0, 2]);
+  });
+
+  it("caso 2", () => {
+    const expected = learn(15, [2, 10, 4, 1]);
+    expect(expected).toEqual([1, 2]);
+  });
+
+  it("caso 3", () => {
+    const expected = learn(25, [10, 15, 20, 5]);
+    expect(expected).toEqual([0, 1]);
+  });
+  it("caso 4", () => {
+    const expected = learn(8, [8, 2, 1]);
+    expect(expected).toEqual([1, 2]);
+  });
+});
