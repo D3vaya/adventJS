9f8a519245a03be8e83201f23eabe42da3d3964c
Jc Ayala <j.ayala.higuera@gmail.com>
2023-03-20 10:27:43 -0300

    feat: challange 18 complete

--- AJS-2021/18/fixFiles.ts
+++ AJS-2021/18/fixFiles.ts
@@ -1,3 +1,25 @@
 export const fixFiles = (files: string[]) => {
-  return []
-}
+  if (!files) throw "undefiend parameter";
+  if (!Array.isArray(files)) throw "parameter is not a array";
+  if (files.length === 0) throw "is empty array";
+  if (typeof files[0] !== "string") throw "array is not a string array";
+
+  const response: string[] = [];
+  const map: Map<string, number> = new Map(files.map((f) => [f, 0]));
+  
+  for (let index = 0; index < files.length; index++) {
+    const counter = map.get(files[index])!;
+    if (counter > 0) {
+      response.push(`${files[index]}(${counter + 1 - 1})`);
+      map.set(files[index], counter + 1);
+    } else {
+      response.push(files[index]);
+      map.set(files[index], counter + 1);
+    }
+  }
+
+  return response;
+};
+
+const files = ["photo", "postcard", "photo", "photo", "video"];
+console.log("[LOG] 🚧 RESPONSE", fixFiles(files));
--- AJS-2021/__test__/fixFiles.test.js
+++ AJS-2021/__test__/fixFiles.test.js
@@ -1,2 +1,45 @@
 import { fixFiles } from "../18/fixFiles.ts";
-describe("Pruebas desafío 18", () => {});
+describe("Pruebas desafío 18", () => {
+  it("Debe lanzar error si el parámetro no viene", () => {
+    expect(() => fixFiles()).toThrowError();
+  });
+
+  it("Debe lanzar error si el arreglo viene vacio", () => {
+    expect(() => fixFiles()).toThrowError();
+  });
+
+  it("Debe lanzar error si el parámetro no es un arreglo de strings", () => {
+    expect(() => fixFiles([1, 2, 3])).toThrowError();
+  });
+});
+
+describe("Casos de prueba", () => {
+  it("Caso prueba 1", () => {
+    const files = ["photo", "postcard", "photo", "photo", "video"];
+    const expected = fixFiles(files);
+    expect(expected).toEqual([
+      "photo",
+      "postcard",
+      "photo(1)",
+      "photo(2)",
+      "video",
+    ]);
+  });
+  it("Caso prueba 2", () => {
+    const files2 = ["file", "file", "file", "game", "game"];
+    const expected = fixFiles(files2);
+    expect(expected).toEqual(["file", "file(1)", "file(2)", "game", "game(1)"]);
+  });
+
+  it("Caso prueba 3", () => {
+    const files3 = ["file", "file(1)", "icon", "icon(1)", "icon(1)"];
+    const expected = fixFiles(files3);
+    expect(expected).toEqual([
+      "file",
+      "file(1)",
+      "icon",
+      "icon(1)",
+      "icon(1)(1)",
+    ]);
+  });
+});
